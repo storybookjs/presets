75c152c4539c41301832e275bbae010d3637768c
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2019-05-31 09:43:47 +0100

    Create story.tsx

---
+++ packages/preset-yarn-pnp-ts/.storybook/story.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Button } from './Button';
+
+interface IComponentWithDocgenInfo {
+  __docgenInfo?: any,
+}
+
+const getDocgen = (component: any) => {
+  return (component as IComponentWithDocgenInfo).__docgenInfo;
+}
+
+storiesOf('Example', module)
+  .add('small', () => <Button variant="small" />)
+  .add('docgenInfo', () => {
+    return (
+      <>
+        <strong>Docgen:</strong>
+        <pre>{JSON.stringify(getDocgen(Button), null, 2)}</pre>
+      </>
+    );
+  });
