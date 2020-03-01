53fae42566e943bbc93d5ec8660de44cacadfd4a
Gaëtan Maisse <gmaisse@solocal.com>
2020-03-01 18:59:01 +0100

    Use spread operator instead of `concat` to be consistent across the package

--- packages/preset-create-react-app/src/index.ts
+++ packages/preset-create-react-app/src/index.ts
@@ -117,7 +117,7 @@ const webpack = (
         join(REACT_SCRIPTS_PATH, 'node_modules'),
         ...getModulePath(CWD),
       ],
-      plugins: plugins.concat([PnpWebpackPlugin]),
+      plugins: [...plugins, PnpWebpackPlugin],
     },
     resolveLoader,
   };
