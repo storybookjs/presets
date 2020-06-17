f34533bb77d718bc5db2936dcad7ff6e0e1f2ee6
Moti Lavian <7mllm7@users.noreply.github.com>
2020-06-17 13:17:02 +0300

    Update presets.js

--- packages/preset-ant-design/.storybook/presets.js
+++ packages/preset-ant-design/.storybook/presets.js
@@ -3,7 +3,6 @@ module.exports = [
     name: '@storybook/preset-ant-design',
     options: {
       lessOptions: {
-        javascriptEnabled: true,
         modifyVars: {
           'primary-color': 'red',
           'border-radius-base': '2px',
