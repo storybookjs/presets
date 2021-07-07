23d3345dab5a9ed59eb2664449e296a9e000e668
Brody McKee <mrmckeb@gmail.com>
2021-07-07 11:36:11 +0300

    feat: remove TS preset

--- examples/ts-vue/.storybook/preview.ts
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue';
-
-import Button from '../src/components/Button/Button.vue';
-
-Vue.component('my-button', Button);
--- examples/ts-vue/src/vue.shims.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.vue' {
-  import Vue from 'vue';
-
-  // eslint-disable-next-line import/no-default-export
-  export default Vue;
-}
